38c80a7b8541f920059ad59f2855f25470332f94
gacreaciones <gacreaciones2024@gmail.com>
2025-06-27 09:21:32 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'una_clave_secreta_muy_segura'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "uri"
-    # Obtener la URI de la base de datos
     uri = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'uri'
