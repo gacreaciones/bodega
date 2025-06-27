e6b34eb0c77dee16787b0af59fbea3def36c6ec4
gacreaciones <gacreaciones2024@gmail.com>
2025-06-26 22:30:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -493,3 +493,11 @@ def eliminar_deuda(id):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
